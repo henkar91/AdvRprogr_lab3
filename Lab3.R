f83fae8d44145ec7cafa4f03b7b1413ce718ac65
Milda Poceviciute <m.poceviciute@gmail.com>
2017-09-07 14:12:36 +0200

    msg

--- Lab3.R
+++ Lab3.R
@@ -1,2 +1,37 @@
 # Lab 3
-# version 1.2
+# 1.1.1 euclidean()
+
+euclidean <- function(a, b) {
+  stopifnot(is.numeric(a), is.numeric(b))
+  while (b != 0) {
+    t <- b
+    b <- a %% b
+    a <- t
+    return(a)
+    
+  }
+}
+
+# 1.1.2 dijkstra()
+Dijkstra <- function(graph, init_node){
+  stopifnot(is.data.frame(graph), length(init_node)==1)
+  Q <- c()
+  # for each vertex v in Graph:             // Initialization
+  # dist[v] ← INFINITY                  // Unknown distance from init node to v
+  # prev[v] ← UNDEFINED                 // Previous node in optimal path from init node
+  # add v to Q                          // All nodes initially in Q (unvisited nodes)
+  # 
+  # dist[init node] ← 0                        // Distance from init node to init node
+  # 
+  # while Q is not empty:
+  #   u ← vertex in Q with min dist[u]    // Node with the least distance will be selected first
+  # remove u from Q 
+  # 
+  # for each neighbor v of u:           // where v is still in Q.
+  # alt ← dist[u] + length(u, v)
+  # if alt < dist[v]:               // A shorter path to v has been found
+  # dist[v] ← alt 
+  # prev[v] ← u 
+  # 
+  # return dist[], prev[]
+}
