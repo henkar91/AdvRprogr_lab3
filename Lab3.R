733e12cff24e58b1579bab7f955fb63549232f02
Henrik Karlsson <henkar91@gmail.com>
2017-09-07 13:48:32 +0200

    task 1.1.1

--- Lab3.R
+++ Lab3.R
@@ -1,2 +1,23 @@
 # Lab 3
-# version 1.2
+# Authors: Henrik Karlsson, Milda Poceviciute
+
+# 1.1 Write R-code
+# 1.1.1 euclidean()
+euclidean <- function(a, b){
+        stopifnot(is.numeric(a) & is.numeric(b))
+        
+        i <- 0
+        while(b != 0){
+                i <- i + 1
+                
+                t <- b
+                b <- a %% b
+                a <- t
+        }
+        return(a)
+}
+
+euclidean(100, 1000)
+
+
+
