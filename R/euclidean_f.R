e8aae391629a3867020f35f10d711d9ed3300238
Henrik Karlsson <henkar91@gmail.com>
2017-09-11 15:30:44 +0200

    extracting package to parent

---
+++ R/euclidean_f.R
@@ -0,0 +1,25 @@
+# Authors: Henrik Karlsson, Milda Poceviciute
+
+#' @title Euclidean Algorithm
+#' @name euclidean
+#' @usage The greatest common divisor of \code{a} and \code{b}.
+#' @param a A number.
+#' @param b A number.
+#' @return A number, which is the greatest common divisor.
+#' @description Euclid's algorithm is an efficient method for computing the greatest common divisor of two numbers. The largest number that divides both of them without leaving a remainder.
+#' @references \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+
+
+# 1.1 Write the R code
+# 1.1.1 euclidean()
+
+euclidean <- function(a, b) {
+  stopifnot(is.numeric(a), is.numeric(b))
+  while (b != 0) {
+    t <- b
+    b <- a %% b
+    a <- t
+    return(a)
+    
+  }
+}
